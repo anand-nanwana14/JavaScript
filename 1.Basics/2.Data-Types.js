3206a94667f603c3f470141c37b5bb2946813ccc
anand-nanwana14 <anandnanwana84@gmail.com>
2024-01-19 13:54:28 +0530

    Data-Types

---
+++ 1.Basics/2.Data-Types.js
@@ -0,0 +1,19 @@
+"use strict" // Treat All code as new js version 
+
+// alert (3+3) we are using nodejs not browser 
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+let Age = 23 
+let Name = "Anand" 
+let isloggedIn = true
+let state = null
+let Number; 
+
+console.table([typeof Age, typeof Name, typeof isloggedIn, typeof state, typeof Number])

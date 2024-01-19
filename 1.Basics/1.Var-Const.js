548fe674e3474d63f21807e00040f618a99bb4e3
anand-nanwana14 <anandnanwana84@gmail.com>
2024-01-19 13:21:18 +0530

    Variables and constants

---
+++ 1.Basics/1.Var-Const.js
@@ -0,0 +1,11 @@
+const Account_Name = "Anand" 
+let Account_ID = "1234"
+var Account_Pass = "123asd"
+let Account_City = "Indore"
+
+console.log(Account_Name)
+
+ Account_City = "Kota"
+ Account_ID = "4455"
+console.table([Account_ID, Account_Name, Account_Pass, Account_City])
+

87953d2f6a7ebbd19870c1cf7da9d556cf7d80bf
Anand Nanwana <121349107+anand-nanwana14@users.noreply.github.com>
2024-01-19 13:25:05 +0530

    1.Var-Const.js

--- 1.Basics/1.Var-Const.js
+++ 1.Basics/1.Var-Const.js
@@ -3,6 +3,10 @@ let Account_ID = "1234"
 var Account_Pass = "123asd"
 let Account_City = "Indore"
 
+/*
+prefer not to use var because of issue in block scope and functional scope 
+*/
+
 console.log(Account_Name)
 
  Account_City = "Kota"

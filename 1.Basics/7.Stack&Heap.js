3f42a39d93d88e542faa8737f771bdfea6f51439
anand-nanwana14 <anandnanwana84@gmail.com>
2024-01-20 08:15:15 +0530

    Stack and Memory

---
+++ 1.Basics/7.Stack&Heap.js
@@ -0,0 +1,19 @@
+let ayush = 33
+
+let manas = ayush
+
+ayush = 76
+console.log(ayush);
+console.log(manas);
+
+let ankit = {
+        email: "ayush@gmail.com",
+        upi: "ayush@ybl"
+}
+
+let nayan = ankit  
+
+nayan.email = "nayan@gmail.com" 
+nayan.upi = "nayan@ybl"
+console.log(nayan);
+console.log(ankit);

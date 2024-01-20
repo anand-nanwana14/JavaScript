d0baef45c5e659e1b09f5743d864818d6f91cce4
anand-nanwana14 <anandnanwana84@gmail.com>
2024-01-20 09:18:22 +0530

    Strings

--- 1.Basics/6.Strings.js
+++ 1.Basics/6.Strings.js
@@ -6,7 +6,7 @@
 // console.log(`Hello my name is ${name}and my repo count is ${repoCount}`);
 
 const gameName = "anand-an-com"
-console.log(gameName.slice(1,8));
+
 
 console.log(gameName[0]);
 console.log(gameName.__proto__);

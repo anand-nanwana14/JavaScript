224458e1e0101ad9eb12b72dc2eab85a716681d5
anand-nanwana14 <anandnanwana84@gmail.com>
2024-01-23 14:20:04 +0530

    Arrow

--- 3.Functions & Parameters/3.arrow.js
+++ 3.Functions & Parameters/3.arrow.js
@@ -1,56 +1,53 @@
-//var c = 300
-let a = 300
-if (true) {
-    let a = 10
-    const b = 20
-    // console.log("INNER: ", a);
-    
-}
+const user = {
+    username: "hitesh",
+    price: 999,
 
-// console.log(a);
-// console.log(b);
-// console.log(c);
-
-
-function one(){
-    const username = "hitesh"
-
-    function two(){
-        const website = "youtube"
-        console.log(username);
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
     }
-    // console.log(website);
-
-     two()
 
 }
 
-// one()
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
 
-if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
-        const website = " youtube"
-        // console.log(username + website);
-    }
-    // console.log(website);
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
 }
 
-// console.log(username);
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+console.log(addTwo(3, 4))
 
 
-// ++++++++++++++++++ interesting ++++++++++++++++++
+// const myArray = [2, 5, 3, 7, 8]
 
-
-console.log(addone(5))
-
-function addone(num){
-    return num + 1
-}
-
-
-
-addTwo(5)
-const addTwo = function(num){
-    return num + 2
-}
+// myArray.forEach()
